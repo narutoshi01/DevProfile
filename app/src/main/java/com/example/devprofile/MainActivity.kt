3cd9e981a3eea33c546b76df70aa95ccb98d9b2d
sakamichi-jewel <taigayyy@gmail.com>
2020-02-14 13:11:26 +0900

    lesson 4

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,12 +1,22 @@
-package com.example.devprofile
+ package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ロゴの角を丸くする
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+//        rounded.isCircular = true : 画像がまん丸になる
+        logo.setImageDrawable(rounded)
     }
 }
